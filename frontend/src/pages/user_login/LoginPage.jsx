ade836024f568611a4528778476e77c14d82a4a6
idul <ludivicobalaterojr@gmail.com>
2024-04-29 01:24:35 +0800

    Login and Application Form Backend

--- frontend/src/pages/user_login/LoginPage.jsx
+++ frontend/src/pages/user_login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import '../pages/user_login/LoginBgm.css';
-import login_background from "../assets/login_background.jpeg"
-
+// import '../../pages/user_login/LoginBgm.css';
+import login_background from "../../assets/login_background.jpeg"
+import LoginCard from "../../pages/user_login/LoginCard"
 export default function LoginPage() {
   return (
     <main className='main-content'>
