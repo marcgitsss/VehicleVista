6e6b12c58d0571c660584b7c357b957804327da7
Robert <amabarobert@gmail.com>
2024-04-29 03:11:36 +0800

    Login Initial Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import LoginCard from "./pages/user_login/LoginCard";
 import Registration from "./pages/vehicle_registration/vehicle_registration"
 import TransitionsModal from "./pages/user_landingpage/landingModal";
+import LoginPage from "./pages/user_login/LoginPage";
 
 function App() {
   return (
     <div >
-      <TransitionsModal />
+      {/* <TransitionsModal /> */}
+
+      <LoginPage />
     </div>
   );
 }
