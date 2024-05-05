470993493ea2c2cd2a6cd880f03746974a9dff3f
Robert <amabarobert@gmail.com>
2024-05-05 13:51:59 +0800

    Registration/Renewal User

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserStatus from './pages/user/UserStatus';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <UserStatus />
   </React.StrictMode>
 );
 
