d63ff74c74584e10b548c1d297c8ecbce01b0f5d
Christzia Marie Atay <Christziamariea@gmail.com>
2024-05-06 16:21:18 +0800

    EmployeeFeatures
    Creating employee_features

---
+++ frontend/src/pages/employee/employee_featurespage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../../components/Navbar/UserHeader'; // Corrected import path for Header
+import Footer from '../../components/Navbar/UserFooter'; // Corrected import path for Footer
+import employeebackgroundImage from '../../assets/SIABackground.png'; // Corrected import path for backgroundImage
+import '../../components/Navbar/UserFooter.css'; // Corrected import path for Footer CSS
+import './employee_featurespage.css';
+
+const EmployeeFeaturePage = () => {
+  
+  return (
+    <section>
+      <Header />
+        
+      <div className='employeefeaturespagebuttons-container'>
+        <button className='dashboardButton'>Dashboard</button>
+        <button className='verifyorcrdButton'>Verify OR/CR and License</button>
+        
+      </div>
+
+      <div className='employeefeaturespagebuttons2-container'>
+        <button className='verifyproofofpaymentButton'>Verify Proof Of Payment</button>
+        <button className='approveapplicationButton'>Approve Application </button>
+      </div>
+
+      <div className='employeefeaturespage'>
+        <img src={employeebackgroundImage} alt='employeebackgroundImage' className='employeebackgroundImage' />
+      </div>
+    
+      <div style={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}>
+        <Footer />
+      </div>
+    </section>
+  );
+};
+
+export default EmployeeFeaturePage;
